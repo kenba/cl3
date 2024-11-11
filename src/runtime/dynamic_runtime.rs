d8dd9d1bf97abe6c9de821167d2418d5584efc11
Dmitry Zakablukov <dimaz@passware.com>
2024-11-11 20:13:21 +0300

    [PKM-2673] Proper way to call optional API functions

--- src/runtime/dynamic_runtime.rs
+++ src/runtime/dynamic_runtime.rs
@@ -2,22 +2,24 @@ pub(crate) use opencl_dynamic_sys::constants as OpenClConstants;
 pub(crate) use opencl_dynamic_sys::types as OpenClTypes;
 
 pub fn is_opencl_runtime_available() -> bool {
-    crate::runtime::load_dynamic_runtime().is_ok()
+    load_dynamic_runtime().is_ok()
 }
 
 pub fn load_dynamic_runtime() -> Result<&'static opencl_dynamic_sys::OpenClRuntime, i32> {
-    const CL_RUNTIME_LOAD_FAILED: i32 = -2000;
-
     opencl_dynamic_sys::load_library()
         .as_ref()
-        .map_err(|_| CL_RUNTIME_LOAD_FAILED)
+        .map_err(|_| opencl_dynamic_sys::constants::CL_RUNTIME_LOAD_FAILED)
 }
 
 macro_rules! cl_call {
     ($func:ident($($arg:expr),* $(,)?)) => {{
-        crate::runtime::load_dynamic_runtime()?.$func($($arg),*)
+        if let Some(result) = crate::runtime::load_dynamic_runtime()?.$func($($arg),*) {
+            result
+        } else {
+            return Err(opencl_dynamic_sys::constants::CL_FUNCTION_NOT_AVAILABLE)
+        }
     }};
     ($namespace:ident::$func:ident($($arg:expr),* $(,)?)) => {{
-        crate::runtime::load_dynamic_runtime()?.$func($($arg),*)
+        cl_call!($func($($arg),*))
     }}
 }
